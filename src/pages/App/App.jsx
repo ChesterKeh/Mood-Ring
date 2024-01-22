f3fd2fc9b0d5de2cc7c29c2a806dc92b816f32d0
Jonit <jonitowt@gmail.com>
2024-01-22 22:09:08 +0800

    added journal components

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import CalendarPage from "../CalendarPage/CalendarPage";
 import { Route, Routes } from "react-router-dom";
+import JournalPage from "../JournalPage/JournalPage";
 
 function App() {
-
   return (
     <>
       <h1>Mood Ring V2</h1>
       <Routes>
-        <Route path="/calendar" element={<CalendarPage/>}/>
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/journal" element={<JournalPage />} />
       </Routes>
     </>
   );
