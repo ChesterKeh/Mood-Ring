07a430a33d7555081dfc54d1db7943b689f3d487
Jonit <jonitowt@gmail.com>
2024-01-23 03:13:42 +0800

    added navbar component

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import CalendarPage from "../CalendarPage/CalendarPage";
 import { Route, Routes } from "react-router-dom";
 import JournalPage from "../JournalPage/JournalPage";
+import Navbar from "../../components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <h1>Mood Ring V2</h1>
       <Routes>
         <Route path="/calendar" element={<CalendarPage />} />
