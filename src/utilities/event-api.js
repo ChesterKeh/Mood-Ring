a349e0539f218f850364db8583b5ff8973a89a29
Brandon Lum <lum371@hotmail.com>
2024-01-23 17:52:37 +0800

    added service call to pull events based on date range

--- src/utilities/event-api.js
+++ src/utilities/event-api.js
@@ -1,5 +1,16 @@
 const BASE_URL = "/api/event";
 
+export async function getEventsByDate(date){
+    const res = await fetch(BASE_URL + "/date/" + Date.parse(date), {
+        headers:{"Content-Type": "application/json" }
+    });
+    if (res.ok) {
+        return res.json();
+    } else {
+        return res;
+    }
+}
+
 export async function createEvent(eventData) {
     const res = await fetch(BASE_URL + "/create", {
         method: "POST",
@@ -10,6 +21,5 @@ export async function createEvent(eventData) {
         return res.json();
     } else {
         return res;
-        // throw new Error("Create Event Error");
     }
 }
