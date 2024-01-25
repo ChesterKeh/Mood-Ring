b30affdebcee87e95416ee25718df0d1fb413142
Chester Keh <114325356+ChesterKeh@users.noreply.github.com>
2024-01-26 02:01:32 +0800

    Delete routes directory

--- routes/subtaskRouter.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const subtaskController = require("../controllers/subtaskController");
-
-router.post("/api/task/:id/subtask", subtaskController.create);
-
-module.exports = router;
